99dfad51342fd94b388db96fc7429416080d0d83
PIerre Morisse <contact@pierremorisse.com>
2021-04-12 17:18:26 +0200

    Fix bug with bamtools lib

--- install.sh
+++ install.sh
@@ -9,11 +9,12 @@ cd build
 cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$PWD/../ ..
 make
 make install
+cd ../
 if [ -d "lib64" ]
 then
 	mv lib64 lib
 fi
 
 # Install LRez
-cd ../../
+cd ../
 make
