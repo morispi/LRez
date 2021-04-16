a5235a58d61d2061ac336d4d5d5935a14d397a72
Marcel Bargull <marcel.bargull@udo.edu>
2021-04-16 19:48:49 +0200

    Makefile: Use some common patterns

--- install.sh
+++ install.sh
@@ -2,19 +2,4 @@
 
 set -e
 
-# Install bamtools
-cd bamtools
-mkdir -p build
-cd build
-cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$PWD/../ ..
-make
-make install
-cd ../
-if [ -d "lib64" ]
-then
-	mv lib64 lib
-fi
-
-# Install LRez
-cd ../
-make
+make -j"$( nproc )" PREFIX="$( pwd )"
